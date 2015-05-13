a628aa28d51f065e44cd1a1f2d10d0eccb42e1c2
Laif Harwood <laifharwood@gmail.com>
2015-05-13 13:05:05 -0600

    implemented checkForNewPeriod function

--- Employee Snacks 2/SharedSwiftCode.swift
+++ Employee Snacks 2/SharedSwiftCode.swift
@@ -8,12 +8,6 @@
 
 import Foundation
 
-func checkForNewPeriod{
-    
-    
-}
-
-
 extension NSDate
 {
     func isGreaterThanDate(dateToCompare : NSDate) -> Bool
@@ -87,3 +81,84 @@ extension NSDate
 */
 }
 
+func checkForNewPeriod(){
+    
+    let now = NSDate()
+    
+    
+    if(now.isGreaterThanDate(periodEndDates[periodEndDates.count - 1])){
+        
+        
+    
+        while (now.isGreaterThanDate(periodEndDates[periodEndDates.count - 1])){
+            
+            let flags: NSCalendarUnit = NSCalendarUnit.CalendarUnitDay | NSCalendarUnit.CalendarUnitMonth | NSCalendarUnit.CalendarUnitYear | NSCalendarUnit.CalendarUnitHour | NSCalendarUnit.CalendarUnitMinute | NSCalendarUnit.CalendarUnitSecond | NSCalendarUnit.CalendarUnitTimeZone
+            
+            
+            let endDateComponents = NSCalendar.currentCalendar().components(flags, fromDate:periodEndDates[periodEndDates.count - 1])
+            
+    
+            let newEndDateComponents = NSDateComponents()
+            let newStartDateComponents = NSDateComponents()
+            
+            if endDateComponents.day == 1{
+                
+                newEndDateComponents.day = 16
+                newEndDateComponents.month = endDateComponents.month
+                newEndDateComponents.year = endDateComponents.year
+                newEndDateComponents.hour = 0
+                newEndDateComponents.minute = 0
+                newEndDateComponents.second = 0
+                newEndDateComponents.timeZone = NSTimeZone.localTimeZone()
+                
+                newStartDateComponents.day = 1
+                newStartDateComponents.month = endDateComponents.month
+                newStartDateComponents.year = endDateComponents.year
+                newStartDateComponents.hour = 0
+                newStartDateComponents.minute = 1
+                newStartDateComponents.second = 0
+                newStartDateComponents.timeZone = NSTimeZone.localTimeZone()
+                
+                
+            }else{
+                
+                newEndDateComponents.day = 1
+                if endDateComponents.month == 12{
+                newEndDateComponents.month = 1
+                newEndDateComponents.year = endDateComponents.year + 1
+                }else{
+                newEndDateComponents.month = endDateComponents.month + 1
+                newEndDateComponents.year = endDateComponents.year
+                }
+                newEndDateComponents.hour = 0
+                newEndDateComponents.minute = 0
+                newEndDateComponents.second = 0
+                newEndDateComponents.timeZone = NSTimeZone.localTimeZone()
+                
+                newStartDateComponents.day = 16
+                newStartDateComponents.month = endDateComponents.month
+                newStartDateComponents.year = endDateComponents.year
+                newStartDateComponents.hour = 0
+                newStartDateComponents.minute = 1
+                newStartDateComponents.second = 0
+                newStartDateComponents.timeZone = NSTimeZone.localTimeZone()
+                
+            }
+            
+            let calendar = NSCalendar(identifier: NSCalendarIdentifierGregorian)
+            let newEndDate = calendar!.dateFromComponents(newEndDateComponents)!
+            let newStartDate = calendar!.dateFromComponents(newStartDateComponents)!
+            
+            periodEndDates.append(newEndDate)
+            periodStartDates.append(newStartDate)
+            
+            periodCharges = []
+            
+            for (key, value) in periodEmployeeDict{
+                periodEmployeeDict[key] = []
+            }
+            
+        }
+    
+    }
+}
